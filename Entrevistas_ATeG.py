a795367a6c6b180312817862efcee9ab1769a2ac
luizg135 <luizgabrielpb@hotmail.com>
2024-11-11 15:44:26 -0300

    Update Entrevistas_ATeG.py

--- Entrevistas_ATeG.py
+++ Entrevistas_ATeG.py
@@ -10,7 +10,7 @@ st.markdown(
         <img src="https://imgur.com/ruDtZT7.png" width="200">
     </div>
     <h1 style="text-align: center; font-size: 50px; font-weight: bold; color: #235937;">Agenda ATeG</h1>
-    <h2 style="text-align: center; font-size: 30px; margin-top: -10px;">Agendamento de Entrevistas</h2>
+    <h2 style="text-align: center; font-size: 30px; margin-top: -35px;">Agendamento de Entrevistas</h2>
     """,
     unsafe_allow_html=True
 )
