3e574c5de5bcd7fdbaa8ec15f5ad47c846e8b844
luizg135 <luizgabrielpb@hotmail.com>
2024-11-11 15:24:38 -0300

    Update Entrevistas_ATeG.py

--- Entrevistas_ATeG.py
+++ Entrevistas_ATeG.py
@@ -67,13 +67,17 @@ else:
         # Interface Streamlit
         # st.title('Agendamento de Entrevistas | ATeG')
 
-st.markdown(
-    """
-    <h1 style="text-align: center; font-size: 50px; font-weight: bold;">Agenda ATeG</h1>
-    <h2 style="text-align: center; font-size: 30px;">Agendamento de Entrevistas</h2>
-    """, 
-    unsafe_allow_html=True
-)
+try:
+    st.markdown(
+        """
+        <h1 style="text-align: center; font-size: 50px; font-weight: bold;">Agenda ATeG</h1>
+        <h2 style="text-align: center; font-size: 30px;">Agendamento de Entrevistas</h2>
+        """, 
+        unsafe_allow_html=True
+    )
+except Exception as e:
+    st.error(f"Erro ao carregar o conteúdo: {e}")
+
         # Formulário de agendamento
         nome = st.text_input('Digite seu nome:')
         data = st.selectbox('Escolha a data:', datas)
