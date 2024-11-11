125fb24e1e6d885d24c8facf186ab8634b17ad48
luizg135 <luizgabrielpb@hotmail.com>
2024-11-11 15:34:04 -0300

    Update Entrevistas_ATeG.py

--- Entrevistas_ATeG.py
+++ Entrevistas_ATeG.py
@@ -12,15 +12,6 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-st.markdown(
-    """
-    <div style="position: fixed; bottom: 10px; left: 50%; transform: translateX(-50%);">
-        <img src="https://drive.google.com/uc?id=1ERjOdoExOsQ7hekZFOJFwiZeyUz4er1c" width="200">
-    </div>
-    """, 
-    unsafe_allow_html=True
-)
-
 # Configuração do Google Sheets
 scope = ["https://spreadsheets.google.com/feeds", 
          "https://www.googleapis.com/auth/spreadsheets", 
