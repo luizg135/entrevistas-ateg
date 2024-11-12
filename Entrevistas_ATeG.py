d844da40c2d602597ca83b6afd3424606bd0ed8a
luizg135 <luizgabrielpb@hotmail.com>
2024-11-12 09:03:08 -0300

    Update Entrevistas_ATeG.py

--- Entrevistas_ATeG.py
+++ Entrevistas_ATeG.py
@@ -16,7 +16,7 @@ st.markdown(
     f"""
     <style>
     .stApp {{
-        background-image: linear-gradient(rgba(255, 255, 255, 0.4), rgba(255, 255, 255, 0.4)), url("https://imgur.com/Mmj3g7Z.png");
+        background-image: linear-gradient(rgba(255, 255, 255, 0.6), rgba(255, 255, 255, 0.6)), url("https://imgur.com/Mmj3g7Z.png");
         background-size: 60%;  /* Ajuste o tamanho da imagem (60% do container) */
         background-position: center;
         background-repeat: no-repeat;
